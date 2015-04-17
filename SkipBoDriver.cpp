a61b0962d995b17102b339581d46a689a7be9cf4
Matthew Ryan Tan <mtan13@ugradx.cs.jhu.edu>
2015-04-17 15:20:02 -0400

    1

---
+++ SkipBoDriver.cpp
@@ -0,0 +1,12 @@
+#include "playerClass.h"
+
+using std::cout;
+using std::cin;
+
+int main(){
+
+  int players;
+  cout << "How many players? " << endl;
+  cin >> players;
+  
+}
---
+++ playerClass.h
@@ -0,0 +1,7 @@
+class player{
+  string name;
+  std::vector<int> hand;
+  std::vector<int> stock;
+  std::vector<int> draw;
+  std::vector<int> discard;
+}
