1840a1c0a38797f31c9663ebfb7a76178aae4c38
JS 600.120 Sp15 <ips1517@ugrad10.cs.jhu.edu>
2015-04-21 20:06:51 -0400

    Finished initial Display implementation

--- Display.cpp
+++ Display.cpp
@@ -132,6 +132,75 @@ static void Display::display(Player player, vector<Build> build, int num) {
 	temp = new string("Discard:");
 	out.push_back(temp);
 	
+	////////////////////////////////////
+	temp = new string(" -1-   -2-   -3-   -4-");
+	out.push_back(temp);
+	
+	////////////////////////////////////
+	temp = new string("┌───┐ ┌───┐ ┌───┐ ┌───┐");
+	out.push_back(temp);
+	
+	////////////////////////////////////
+	temp = new string("│ ");
+	temp += convert(player.discard[0].getTop());
+	if (player.discard[0].getTop() >= 10) {
+		temp += "│ │ ";
+	}
+	else temp += " │ │ ";
+	temp += convert(player.discard[1].getTop());
+	if (player.discard[1].getTop() >= 10) {
+		temp += "│ │ ";
+	}
+	else temp += " │ │ ";
+	temp += convert(player.discard[2].getTop());
+	if (player.discard[2].getTop() >= 10) {
+		temp += "│ │ ";
+	}
+	else temp += " │ │ ";
+	temp += convert(player.discard[3].getTop());
+	if (player.discard[3].getTop() >= 10) {
+		temp += "│";
+	}
+	else temp += " │";
+	out.push_back(temp);
+	
+	////////////////////////////////////
+	temp = new string("└───┘ └───┘ └───┘ └───┘");
+	out.push_back(temp);
+	
+	////////////////////////////////////
+	temp = new string("Stockpile:");
+	out.push_back(temp);
+	
+	////////////////////////////////////
+	temp = new string("┌───┐");
+	out.push_back(temp);
+	
+	////////////////////////////////////
+	temp = new string("│ ");
+	temp += convert(player.stock.getTop());
+	if (player.stock.getTop() >= 10) {
+		temp += "│ ";
+	}
+	else temp += " │ ";
+	temp += convert(player.stock.getSize());
+	temp += " cards left"
+	
+	////////////////////////////////////
+	temp = new string("└───┘");
+	out.push_back(temp);
+	
+	////////////////////////////////////
+	temp = new string("└Your Move: ");
+	out.push_back(temp);
+	
+	
+	for (int i = 0; i < out.size(); ++i) {
+		if (i == out.size()) {
+			cout << out.at(i);
+		}
+		else cout << out.at(i) << endl;
+	}
 }
 
 static string Display::convert(int num) {
