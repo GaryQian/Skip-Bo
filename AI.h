1e16d2033b7a378a7d3cf2261bf0214286070361
Kathleen Kusworo <kathleenkusworo@gmail.com>
2015-04-23 06:57:34 +0000

    AI.h edited online with Bitbucket

--- AI.h
+++ AI.h
@@ -1,5 +1,21 @@
-#include "Player.h"
-
-class AI: public Player {
-
-};
+ #include "Player.h"
++#include "Draw.h"
++#include "Build.h"
++#include "Deck.h"
++#include "Display.h"
++#include <string>
++#include <vector>
++
+Add a comment to this line
++using std::string;
++using std::vector;
+ 
+ class AI: public Player {
+ 
++	AI(string name, Draw* draw, vector<Build>* build);
++	
++	string calculateMove();
++	
++	bool contains(vector<int> vec, int num);
++	
+ };
