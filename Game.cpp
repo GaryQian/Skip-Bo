646bfa83f4beaf393a366370dc61a04e388cb5cb
a <a@as-MacBook-Air.local>
2015-04-23 16:52:22 -0400

    Added processing for user input

--- Game.cpp
+++ Game.cpp
@@ -6,6 +6,7 @@ using std::cout;
 using std::endl;
 using std::vector;
 using std::string;
+using std::getline;
 
 Game::Game(vector<string> names){
   Draw d = new Draw(); 
@@ -83,3 +84,139 @@ void Game::save_game(string filename){
   outFile << turn << endl;
 }
 
+void Game::load_game(string filename){
+
+  std::ifstream inFile(filename);
+  int num;
+  int numPlayers;
+  int numMoves;
+  string name;
+  vector<int> hand;
+  vector<int> set;
+  vector<vector<int>> discard;
+  vector<int> stock;
+
+
+  inFile > numPlayers;
+
+  while((inFile > num) != -1){
+    draw.push_back(num);
+  }
+
+  for(int i = 0; i < 4; i++){
+    while((inFile>num) != -1){
+      build[i].push_back(num);
+    }
+  }
+
+  build.resize(4);
+
+  for(int i = 0; i < numPlayers; i++){
+    inFile > name;
+
+    while((inFile > num)!= -1){
+      hand.push_back(num);
+    }
+
+    for(int i = 0; i < 4;){
+      if((inFile > num) != -1){
+        discard[i].push_back(num);
+      }
+      else{
+        ++i;
+      }
+    }
+
+    while((inFile > num)!= -1){
+      stock.push_back(num);
+    }
+
+    players.push_back(new Player(name, hand, discard, stock));
+    hand.clear();
+    discard.clear();
+    stock.clear();
+  }
+
+  inFile >> numMoves;
+
+  for(int i = 0; i < numMoves; i++){
+    Move m;
+    inFile >> m.playerNum;
+    inFile >> m.source;
+    inFile >> m.dest;
+    move.push_back(m);
+  }
+
+  inFile >> turn;
+}
+
+void Game::process(string input){
+  if (input.length() < 4){
+    cout << "Invalid input" << endl;
+    return;
+  }
+
+  string source = input.at(0);
+  Move m;
+
+  if (source == 's'){
+    input = input.substr(1);
+    m.source == s;
+    m.index = 0;
+    m.value = players[turn%players.size()].getStock()[0];
+  }
+  else if (source == 'd'){
+    m.source = source;
+    m.index = (input.at(1) - '0')-1;
+    if (index > 3){
+      cout << "invalid index" << endl;
+      return;
+    }
+    input = input.substr(2);
+    m.value = players[turn%players.size()].getDiscard()[m.index][0];
+  }
+  else if (source == 'h'){
+    m.source = source;
+    m.index = (input.at(1) - '0')-1;
+    if (index > 4){
+      cout << "invalid index" << endl;
+      return;
+    }
+    input = input.substr(2);
+    m.value = players[turn%players.size()].getHand()[m.index];
+  }
+  else{
+    cout << "Invalid card source" << endl;
+    return;
+  }
+
+  if (input.at(0) != " "){
+    cout << "source and destination must be separated with a single whitespace"\
+	 << endl;
+    return;
+  }
+
+  string dest  = input.substr(1,0);
+  int destIndex = 0;
+
+  if (dest == 'b' || dest == 'd'){
+    m.dest = dest;
+    destIndex = (input.at(2) - '0')-1;
+
+    if (destIndex > 3){
+      cout << "Invalid index " << endl;
+    }
+
+    m.destIndex = destIndex;
+  }
+  else{
+    cout << "Invalid destination" << endl;
+  }
+
+  this.play(m);
+  return;
+}
+
+void Game::play(Move m){
+
+}
