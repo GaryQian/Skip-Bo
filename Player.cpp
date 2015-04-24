8b825c98357e6a293920b5d5795e373b28aef384
JS 600.120 Sp15 <ips153@ugradx.cs.jhu.edu>
2015-04-23 22:36:56 -0400

    Minor modification of player methods, fixed indentation

--- Player.cpp
+++ Player.cpp
@@ -3,6 +3,9 @@
 #include <string>
 #include <vector>
 
+#define LESSPLAYERS 30
+#define MOREPLAYERS 20
+
 using std::string;
 using std::vector;
 
@@ -17,57 +20,54 @@ Player::Player(string name, Draw* draw, vector<Build>* build) {
 //do we have to accomodate for when there are more than 4 players; in which there would be 20 cards/player?
 void Player::deal(int playerNum) 
 {
-  if (playerNum <= 4 && playerNum >= 1)
-    {
-      stock += draw.take(30);
-    }
-  if (playerNum > 4)
-    {
-      stock += draw.take(20);
-    }
+  //gives each player 30 cards if there are less than or equal to 4 players
+  if (playerNum <= 4 && playerNum >= 1) {
+      stock += draw.take(LESSPLAYERS);
+  }
+
+  //gives each player 20 cards if there are over 4 players
+  if (playerNum > 4) {
+    stock += draw.take(MOREPLAYERS);
+  }
 }
 
-bool Player::hasWon()
-{
-  if (stock.isEmpty())
+bool Player::hasWon() {
+  if (stock.isEmpty()) {
     return true;
+  }
   return false;
 }
 
-bool Player::playCard(Deck a, Deck b)
-{
-  if (a.move(b))
+bool Player::playCard(Deck a, Deck b) {
+  if (a.move(b)) {
     return true;
+  }
   return false;
 }
 
-bool Player::playCard(Deck b, int num)
-{
-  if (hand.move(Deck b, int num))
+bool Player::playCard(Deck b, int num) {
+  if (hand.move(Deck b, int num)) {
     return true;
+  }
   return false;
 }
 
-string Player::getName()
-{
+string Player::getName() {
   return name;
 }
 
-Hand Player::getHand()
-{
+Hand Player::getHand() {
   return hand;
 }
 
-vector<Discard> Player::getDiscard()
-{
+vector<Discard> Player::getDiscard() {
   return discard;
 }
 
-vector<int> Player::getStock()
-{
+vector<int> Player::getStock() {
   return stock;
 }
 
 bool Player::isAI() {
-	return isAI;
+  return isAI;
 }
