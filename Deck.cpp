d4c4edbb1f59a65a5668106f61b244f600e8ec94
JS 600.120 Sp15 <ips153@ugradx.cs.jhu.edu>
2015-04-23 23:45:47 -0400

    Debugging

--- Deck.cpp
+++ Deck.cpp
@@ -11,7 +11,7 @@ int Deck::getSize() {
   return cards.size();
 }
 
-int Deck::takeCard(){
+int Deck::takeCard() {
   int topCard = getTop();
   cards.pop_back();
   return topCard;
@@ -21,15 +21,15 @@ int Deck::getTop() {
   return cards[getSize() - 1];
 }
  
-bool Deck::isEmpty(){
+bool Deck::isEmpty() {
   return getSize() == 0;
 }
 
-void Deck::operator +=(int value){
+void Deck::operator +=(int value) {
   cards.push_back(value);
 }
 
-void Deck::operator +=(vector<int> list){
+void Deck::operator +=(vector<int> list) {
   vector<int> newCards;
   newCards.reserve(getSize() + list.size());
   newCards.insert(newCards.end(), cards.begin(), cards.end());
@@ -37,17 +37,17 @@ void Deck::operator +=(vector<int> list){
   cards = newCards;
 }
 
-string Deck::toString() const{
+string Deck::toString() const {
   vector<int>::const_iterator it;
   ostringstream oss;
 
-  for(it = cards.begin(); it < cards.end(); it++){
+  for(it = cards.begin(); it < cards.end(); it++) {
     oss << *it << " ";
   }
   return oss.str();
 }
 
-ostream& operator << (ostream& os, const Deck& d){
+std::ostream& operator << (std::ostream& os, const Deck& d) {
   os << d.toString();
   return os;
 }
