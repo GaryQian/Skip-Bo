c455def208e8c284b51d9dafbead26c3fa6d2520
JS 600.120 Sp15 <ips153@ugradx.cs.jhu.edu>
2015-04-26 18:43:18 -0400

    Methods commented

--- Player.cpp
+++ Player.cpp
@@ -6,6 +6,7 @@
 using std::string;
 using std::vector;
 
+//Constructors for player
 Player::Player(string name, Draw* draw, vector<Build>* build, Stock stock) {
   this->draw = draw;
   this->name = name;
@@ -28,22 +29,23 @@ Player::Player(string name, Draw* draw, vector<Build>* build, Stock stock, Hand 
   isAnAI = false;
 }
 
-//How to know from Move data which discard/build pile to put dest. card into?
 bool Player::move(Move yourMove) {
-
-  //////////IMPLEMENTED////////////
   //source card is stockpile
   if (yourMove.source == 's') {  
+    //destination of card is build 
     if (yourMove.dest == 'b') {
       //Loops until the top card of build is skip-bo or the current-card - 1
       int i = 0;
       while((build->at(i).getTop() != (yourMove.value - 1)) || (build->at(i).getTop() != 0)) {
 	i++;
       }
+      //Moves the card to the correct build pile
       stock.move(build->at(i));
     }
+    //destination of card is discard
     else if (yourMove.dest == 'd') {
-      stock.move(discard.at(yourMove.destIndex));
+      //Moves the card to the discard pile provided by user 
+     stock.move(discard.at(yourMove.destIndex));
     }
   }
 
@@ -54,28 +56,31 @@ bool Player::move(Move yourMove) {
     while((build->at(i).getTop() != (yourMove.value - 1)) || (build->at(i).getTop() != 0)) {
       i++;
     }
-    //Moves from the particular discard pile to the chosen build pile
+    //Moves from the discard pile provided by user to the correct build pile
     discard.at(yourMove.index).move(build->at(i));
   }
-
-  ///////////NOT IMPLEMENTED YET//////////
 
   //source card is hand 
   else {
     if (yourMove.dest == 'b') {
-    int i = 0;
-    while((build->at(i).getTop() != (yourMove.value - 1)) || (build->at(i).getTop() != 0)) {
-      i++;
-    }
-    hand.move(build->at(i), yourMove.index);
+      int i = 0;
+      while((build->at(i).getTop() != (yourMove.value - 1)) || (build->at(i).getTop() != 0)) {
+	i++;
+      }
+      //Moves from hand card provided by user to the correct build pile  
+      hand.move(build->at(i), yourMove.index);
     }
     else if (yourMove.dest == 'd') {
+      //Moves from the hand card to the discard pile whose index is provided by the user
       hand.move(discard.at(yourMove.destIndex), yourMove.index);
     }
   }
+
+  //all exceptions should have been handled if this method is called, so there is no need to return false
   return true;
 }
 
+//Returns if the player has won
 bool Player::hasWon() {
   if (stock.isEmpty()) {
     return true;
@@ -83,6 +88,7 @@ bool Player::hasWon() {
   return false;
 }
 
+//These methods are not necessary anymore
 /*
 bool Player::playCard(Deck a, Deck b) {
   if (a.move(b)) {
@@ -98,6 +104,8 @@ bool Player::playCard(Deck b, int num) {
   return false;
 }
 */
+
+//////////Accessor methods///////////
 
 string Player::getName() {
   return name;
