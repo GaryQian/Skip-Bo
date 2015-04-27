572f6e7177d950c01c7565954b59885cfc2b028d
Gary Qian <gary@qian.ninja>
2015-04-27 19:18:03 +0000

    AI.cpp created online with Bitbucket

---
+++ AI.cpp
@@ -0,0 +1,118 @@
+#include "Player.h"
+#include "Deck.h"
+#include "Display.h"
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+
+using std::string;
+using std::vector;
+using std::cout;
+using std::endl;
+
+AI::AI(string name, Draw* draw, vector<Build>* build, Stock stock) {
+	this->draw = draw;
+	this->name = name;
+	this->build = build;
+	this->stock = stock;
+  
+
+	seed = time(NULL);
+	isAnAI = true;
+}
+
+AI::AI(string name, Draw* draw, vector<Build>* build, Stock stock, Hand hand, vector<Discard> discard){
+  this->draw = draw;
+  this->name = name;
+  this->build = build;
+  this->stock = stock;
+  this->hand = hand;
+  this->discard = discard;
+  seed = time(NULL);
+  isAnAI = true;
+}
+
+string AI::getMove() {
+	Display d;
+	vector<int> validNums;
+	for (int i = 0; i < 4; ++i) {
+		if (build->at(i).getTop() != -1) {
+			validNums.push_back(build->at(i).getTop() + 1);
+		}
+	}
+	vector<string> moves;
+	string* temp;
+	for (int i = 0; i < hand.getSize(); ++i) {
+		if (contains(validNums, hand.at(i))) {
+			temp = new string("h");
+			*temp += d.convert(i + 1);
+			*temp += " b";
+			*temp += d.convert(find(validNums, hand.at(i)));
+			moves.push_back(*temp);
+		}
+	}
+	
+	for (int i = 0; i < 4; ++i) {
+		if (contains(validNums, discard[i].getTop())) {
+			temp = new string("d");
+			*temp += d.convert(i + 1);
+			*temp += " b";
+			*temp += d.convert(find(validNums, hand.at(i)));
+			moves.push_back(*temp);
+		}
+	}
+	
+	if (contains(validNums, stock.getTop())) {
+		temp = new string("s");
+		*temp += " b";
+		for (int i = 0; i < 5; i++) {
+			if (hand.at(i) == stock.getTop())
+				*temp += d.convert(find(validNums, hand.at(i)));
+			break;
+		}
+		moves.push_back(*temp);
+	}
+	
+	//PICK RANDOM MOVE/BEST MOVE HERE
+	srand(seed);
+	seed++;
+	int ran = rand() % moves.size();
+	string keep = string(moves.at(ran));
+	for (int i = 0; i < (int) moves.size(); ++i) {
+		delete &moves.at(i);
+	}
+	//wait so that AI does not move instantaneously
+	cout << moves.at(ran) << endl;
+	
+	std::this_thread::sleep_for(std::chrono::milliseconds(300));	
+	
+	return moves.at(ran);
+	
+	
+	///////////////
+}
+
+bool AI::contains(vector<int> vec, int num) {
+	if (num == 0) return true;
+	for (int j = 0; j < (int) vec.size(); ++j) {
+		if (num == vec.at(j)) {
+			return true;
+		}
+	}
+	return false;
+}
+
+int AI::find(vector<int> vec, int num) {
+	if (num == 0) return -1;
+	for (int j = 0; j < (int) vec.size(); ++j) {
+		if (num == vec.at(j)) {
+			return j;
+		}
+	}
+	return -1;
+}
