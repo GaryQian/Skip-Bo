04151659754d1b3f3148edeaa095e4a73082f5c8
JS 600.120 Sp15 <ips1517@ugrad10.cs.jhu.edu>
2015-04-30 00:37:27 -0400

    Began AI and Display Testing

---
+++ AITest.cpp
@@ -0,0 +1,12 @@
+#include "Player.h"
+#include "Deck.h"
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <cstdio>
+
+using std::string;
+using std::cout;
+using std::vector;
+using std::endl;
---
+++ DisplayTest.cpp
@@ -0,0 +1,12 @@
+#include "Display.h"
+#include "Deck.h"
+#include "Player.h"
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cstdio>
+
+using std::string;
+using std::cout;
+using std::vector;
+using std::endl;
