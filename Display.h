29c72d9da23023e3201e92f641f336d0344a8813
JS 600.120 Sp15 <ips1517@ugrad10.cs.jhu.edu>
2015-05-01 14:43:01 -0400

    comments!

--- Display.h
+++ Display.h
@@ -5,22 +5,35 @@
 using std::vector;
 
 class Display {
-  friend class AI;
-  friend class DisplayTest;
-  
-  vector<int> buildTop;
-  
- public:
-  void display(Player* player, vector<Build*> build, int num);
-  
-  void display(AI player, vector<Build*> build, int num); 
-  
-  void change(Player* player, int num);
-  
-  void intro();
-  
- private:
-  string convert(int num);
-  string convert(int num, Build build);
+	friend class AI;
+	friend class DisplayTest;
+
+public:
+	/*
+	Displays the game board from all the parameters
+	*/
+	void display(Player* player, vector<Build*> build, int num);
+	/*
+	Displays the game board from all the parameters if the player passed in is an AI
+	*/
+	void display(AI player, vector<Build*> build, int num); 
+	/*
+	Displays the Change player screen
+	*/
+	void change(Player* player, int num);
+	/*
+	Displays the intro title Logo
+	*/
+	void intro();
+
+private:
+	/*
+	Helper method that takes in a number and converts it into a string of that number
+	*/
+	string convert(int num);
+	/*
+	Helper method that takes in a number and Build Pile and returns the top card, ignoring skipbo cards
+	*/
+	string convert(int num, Build build);
 
 };
