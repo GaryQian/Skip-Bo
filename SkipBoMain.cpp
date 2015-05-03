8734e31b710619ed7b6ec926d6f84d368173aa43
Kathleen Kusworo <kkuswor1@ugrad2.cs.jhu.edu>
2015-05-03 14:27:15 -0400

    Changed indentation

--- SkipBoMain.cpp
+++ SkipBoMain.cpp
@@ -20,92 +20,92 @@ using std::string;
 using std::exception;
 
 int main(){
-	Game* game;
-	string save;
-	string input;
-	vector<string> names;
-	int players;
-	Display d;
-	//Prompts user to enter in a game
-	cout << "Welcome to Skip-Bo!" << endl;
-	d.intro();
-	cout << "Would you like to load a saved game?" << endl;
+  Game* game;
+  string save;
+  string input;
+  vector<string> names;
+  int players;
+  Display d;
+  //Prompts user to enter in a game
+  cout << "Welcome to Skip-Bo!" << endl;
+  d.intro();
+  cout << "Would you like to load a saved game?" << endl;
 
-	//Loads the game	
-	cin >> save;
-	std::transform(save.begin(), save.end(), save.begin(), ::tolower);
-	if (save == "yes" || save == "y" || save == "ye" || save == "1" || save.at(0) == 'y' || save.at(0) == '1') {
-		cout << "Please enter the savefile name:" << endl;
-		cin >> input;
-		game = new Game();
-		game->load_game(input);
+  //Loads the game	
+  cin >> save;
+  std::transform(save.begin(), save.end(), save.begin(), ::tolower);
+  if (save == "yes" || save == "y" || save == "ye" || save == "1" || save.at(0) == 'y' || save.at(0) == '1') {
+    cout << "Please enter the savefile name:" << endl;
+    cin >> input;
+    game = new Game();
+    game->load_game(input);
+  }
+  else {
+    //Else, begins a new game
+		
+    cout << "How many players? (Enter Integer):" << endl;
+    cin >> players;
+		
+    names.resize(players);
+		
+    for(int i = 0; i < players; i++){
+      cout << "Enter name of player " << i+1 << ". If player is an AI, prefix with \"AI\"." << endl;
+      cin >> names[i];
+    }
+		
+    game = new Game(names);
+    game->nextTurn();
+    game->refill();
+  }
+
+  cin.ignore();
+
+  //while game hasn't ended
+  while (!game->hasEnded()) {
+    try{
+      d.display(game->getPlayer(), game->getBuild(), game->getPlayerNumber());
+      vector<Move*> choices = game->canMove();
+      while (choices.size()) {
+	try{  
+	  for(unsigned long i = 0; i < choices.size(); i++){
+	    delete choices.at(i);
+	  }
+	  input = game->getPlayer()->getMove();
+	  game->process(input);
+	  d.display(game->getPlayer(), game->getBuild(), game->getPlayerNumber());
 	}
-	else {
-		//Else, begins a new game
-		
-		cout << "How many players? (Enter Integer):" << endl;
-		cin >> players;
-		
-		names.resize(players);
-		
-		for(int i = 0; i < players; i++){
-			cout << "Enter name of player " << i+1 << ". If player is an AI, prefix with \"AI\"." << endl;
-			cin >> names[i];
-		}
-		
-		game = new Game(names);
-		game->nextTurn();
-		game->refill();
+	catch(exception& e){
+	  cout << e.what() << endl;
 	}
-
-	cin.ignore();
-
-
-	while (!game->hasEnded()) {
-		try{
-			d.display(game->getPlayer(), game->getBuild(), game->getPlayerNumber());
-			vector<Move*> choices = game->canMove();
-			while (choices.size()) {
-				try{  
-					for(unsigned long i = 0; i < choices.size(); i++){
-						delete choices.at(i);
-					}
-					input = game->getPlayer()->getMove();
-					game->process(input);
-					d.display(game->getPlayer(), game->getBuild(), game->getPlayerNumber());
-				}
-				catch(exception& e){
-					cout << e.what() << endl;
-				}
-				if (game->getPlayer()->getHand().getSize()==0){
-				  game->refill();
-				}
-				choices = game->canMove();
-			}
-			d.display(game->getPlayer(), game->getBuild(), game->getPlayerNumber());   
-			while(true){
-				cout << "No moves left.\nPlease move card to discard pile to end turn\n";	
-				input= game->getPlayer()->getMove();
-				try{
-					game->process(input);
-				}
-				catch (exception& e){
-					cout << e.what() << endl;
-				}
-			}
-		}
-		catch (int a){
-			d.display(game->getPlayer(), game->getBuild(), game->getPlayerNumber());
-			//d.change(game->getNextPlayer(), game->getNextPlayerNumber());
-			cout << "Turn end\n" << endl;
-			game->nextTurn();
-			game->refill();
-		}
-		catch (char c){
-			cout << "File successfully saved\n" << endl;
-			delete game;
-			return 0;
-		}
+	if (game->getPlayer()->getHand().getSize()==0){
+	  game->refill();
 	}
-	delete game;
+	choices = game->canMove();
+      }
+      d.display(game->getPlayer(), game->getBuild(), game->getPlayerNumber());   
+      while(true){
+	cout << "No moves left.\nPlease move card to discard pile to end turn\n";	
+	input= game->getPlayer()->getMove();
+	try{
+	  game->process(input);
+	}
+	catch (exception& e){
+	  cout << e.what() << endl;
+	}
+      }
+    }
+    catch (int a){
+      d.display(game->getPlayer(), game->getBuild(), game->getPlayerNumber());
+      //d.change(game->getNextPlayer(), game->getNextPlayerNumber());
+      cout << "Turn end\n" << endl;
+      game->nextTurn();
+      game->refill();
+    }
+    catch (char c){
+      cout << "File successfully saved\n" << endl;
+      delete game;
+      return 0;
+    }
+  }
+  delete game;
 }
