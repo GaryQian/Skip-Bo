949ac71f427ff8855b5b65d4a0d5c026dd54d01b
Kathleen Kusworo <kkuswor1@ugrad2.cs.jhu.edu>
2015-05-04 16:36:59 -0400

    Code that generates test_speed.txt. Modify however you want

---
+++ testGen.cpp
@@ -0,0 +1,29 @@
+#include <fstream>
+#include <iostream>
+
+using std::ofstream;
+using std::endl;
+
+int main(void){
+  ofstream outfile("test_speed.txt");
+  
+  outfile << "no" << endl;
+  outfile << "2" << endl;
+  outfile << "me" << endl;
+  outfile << "you" << endl;
+
+  for(int i = 0; i < 300; i++){
+    outfile << "h1 d1" << endl;
+  }
+
+  for(int i = 0; i < 300; i++){
+    outfile << "undo" << endl;
+  }
+
+  for(int i = 0; i < 300; i++){
+    outfile << "redo" << endl;
+  }
+
+  outfile.close();
+  
+}
